c6816c6e9b4402bd016249f9dffb7e6ba739120f
v.malikov <v.malikov@bilaboratory.com>
2025-04-27 09:27:27 +0300

    Added config in gitignore for vscode. Added html-templates for components <header> and <footer>. Added tets data for render templates.

---
+++ test_data.py
@@ -0,0 +1,38 @@
+# Перечень товаров для каталога товаров
+PRODUCTS_LIST = [
+    {
+        "id": "1",
+        "name": "Товар 1",
+        "description": "Описание товара 1",
+        "price": 1000,
+        "image_url": "https://via.placeholder.com/200x200.png?text=product1",
+    },
+    {
+        "id": "2",
+        "name": "Товар 2",
+        "description": "Описание товара 2",
+        "price": 1500,
+        "image_url": "https://via.placeholder.com/200x200.png?text=product2",
+    },
+    {
+        "id": "3",
+        "name": "Товар 3",
+        "description": "Описание товара 3",
+        "price": 980,
+        "image_url": "https://via.placeholder.com/200x200.png?text=product3",
+    },
+    {
+        "id": "4",
+        "name": "Товар 4",
+        "description": "Описание товара 4",
+        "price": 1200,
+        "image_url": "https://via.placeholder.com/200x200.png?text=product4",
+    },
+    {
+        "id": "5",
+        "name": "Товар 5",
+        "description": "Описание товара 5",
+        "price": 2000,
+        "image_url": "https://via.placeholder.com/200x200.png?text=product5",
+    },
+]
